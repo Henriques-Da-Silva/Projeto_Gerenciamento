f1f9ae5e0f55458079105651e0adfe903e5df48e
Henrique Da Silva <henriquehenry533@gmail.com>
2025-02-03 01:01:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+'''Restrições: o estoque deve ser maior que dez para permitir a venda, garanta que se for 
+digitado número no nome da fruta, o prgrama não adicione a fruta e o nome deve ter pelo 
+menos 3 letras, o preço não pode ser negativo e deve estar em KZ, não pode ser adicionado 
+um número negativo ao estoque, o programa só pára se o usuário quiser, trata todo tipo de 
+Exceções se houverem, não pode haver duas frutas iguais na lista. '''
+
 frutas = [{'Nome': 'Maça', 'Preco': 300, 'Quantidade': 15, 'Total Vendido': 0},
           {'Nome': 'Manga', 'Preco': 100, 'Quantidade': 10, 'Total Vendido': 0},
           {'Nome': 'Banana', 'Preco': 200, 'Quantidade': 13, 'Total Vendido': 0},
@@ -8,3 +14,32 @@ frutas = [{'Nome': 'Maça', 'Preco': 300, 'Quantidade': 15, 'Total Vendido': 0},
           {'Nome': 'Melancia', 'Preco': 600, 'Quantidade': 15, 'Total Vendido': 0},
           {'Nome': 'Amora', 'Preco': 500, 'Quantidade': 10, 'Total Vendido': 0},
           {'Nome': 'Kiwi', 'Preco': 400, 'Quantidade': 14, 'Total Vendido': 0}]
+
+class app():
+
+    def main():
+        print
+
+    def adicionar_fruta():
+        print
+        
+    def remover_fruta():
+        print
+        
+    def adicionar_quantidade_de_uma_fruta():
+        print
+        
+    def vender():
+        print
+        
+    def buscar_frutas_por_nome():
+        print
+        
+    def relatorios():
+        # Listar os dados de todas as frutas
+        
+        # Total Pressuposto com as vendas
+        
+        # Total adquirido até agora
+        
+        print
